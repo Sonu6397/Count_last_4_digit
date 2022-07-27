ba0477b99cdaaa863d26c5674595e69ceb08fa3b
admin <admin@CHETUIWK1860>
2022-07-27 15:39:27 +0530

    Add project files.

---
+++ Count_string/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Count_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr =  { 1, 4, 34, 5, 9, 4, 23, };
+
+           for(int i=arr.Length-1; i>2; i--)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.ReadLine();
+    }   }
+}
